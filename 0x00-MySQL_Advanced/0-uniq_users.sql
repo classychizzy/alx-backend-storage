2c5b41fa3ad87c144821953cae23dcd7a1f8959a
classychizzy <chizobachukwuoma@gmail.com>
2023-01-12 21:49:38 +0000

    bug fix

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,8 @@
 -- script to create table users if it doesn't exist
 --and add unique constraint to email column
-CREATE TABLE IF NOT EXISTS users(
+CREATE TABLE IF NOT EXISTS users (
 	Id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL,
 	UNIQUE (email),
 	name VARCHAR(255)
-)
+);
